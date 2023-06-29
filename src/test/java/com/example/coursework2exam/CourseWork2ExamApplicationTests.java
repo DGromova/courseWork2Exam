d12abd2f4a02994c89d8e78809b5360cf1dad32f
Daria Gromova <igromova81@gmail.com>
2023-06-29 21:25:25 +0300

    Initial commit

---
+++ src/test/java/com/example/coursework2exam/CourseWork2ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursework2exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

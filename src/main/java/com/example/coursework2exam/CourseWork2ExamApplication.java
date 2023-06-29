3d7671d319d5e5fec9b3c19aca2924de3041e482
Daria Gromova <igromova81@gmail.com>
2023-06-29 21:26:58 +0300

    start cw

--- src/main/java/com/example/coursework2exam/CourseWork2ExamApplication.java
+++ src/main/java/com/example/coursework2exam/CourseWork2ExamApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CourseWork2ExamApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CourseWork2ExamApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWork2ExamApplication.class, args);
+    }
 
 }
